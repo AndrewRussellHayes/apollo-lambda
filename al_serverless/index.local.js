cfd0f755178516e61a2e6960b90252d556fa562f
Lewis Daly <lewis.daly@enabled.com.au>
2017-07-23 12:56:34 +0930

    Basic local working

--- al_serverless/index.local.js
+++ al_serverless/index.local.js
@@ -6,12 +6,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlExpress = require('apollo-server-express').graphqlExpress;
 
+console.log("HEY!!");
+
 const schema = require('./Schema');
 const PORT = 3000;
 
 const app = express();
 
-// bodyParser is needed just for POST.
+app.get('/', function(req, res) {
+  res.send("Hello graphql");
+});
+
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schema }));
 
-app.listen(PORT);
+app.listen(PORT, function() {
+  console.log("server listening on port " + PORT);
+});
